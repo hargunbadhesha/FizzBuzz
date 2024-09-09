d1e063e392330147ba3237630f98388cb69a627b
hargun.badhesha <hargunb2021@gmail.com>
2024-09-09 14:32:37 -0400

    multiples.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,14 +1,14 @@
 public class Multiples {
-    public static void main(String[] args) {
+    public static int main(int n, int a, int b) {
         int i = 1;
         int answer = 0;
-        while(i<1000){
-            if(i%3==0 || i%5==0){
+        while(i<n){
+            if(i%a==0 || i%b==0){
                 answer = answer + 1;
             }
             i++;
 
         }
-        System.out.println(answer);
+        return answer;
     }
 }

c52fa80f907dae0a7b930dd99cd73f0a66058546
hargun.badhesha <hargunb2021@gmail.com>
2024-09-09 10:45:10 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,32 +3,65 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        //for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+           // boolean divisibleBy3 = i % 3 == 0;
+            //boolean divisibleBy5 = i % 5 == 0;
 
             // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+            //if (divisibleBy3 && divisibleBy5) {
 
-                System.out.println("Fizz Buzz");
+                //System.out.println("Fizz Buzz");
 
-            } else if (divisibleBy3) {
+            //} else if (divisibleBy3) {
 
-                System.out.println("Fizz");
+                //System.out.println("Fizz");
 
-            } else if (divisibleBy5) {
+           // } else if (divisibleBy5) {
 
-                System.out.println("Buzz");
+                //System.out.println("Buzz");
 
-            } else {
+           // } else {
 
-                System.out.println(i);
+                //System.out.println(i);
 
-            }
+            //}
+        //}
+   // }
+//}
+
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<100){
+            i = getI(i);
         }
+        }
+
+    private static int getI(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 }

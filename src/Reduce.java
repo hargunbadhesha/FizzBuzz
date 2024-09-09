e6440a0df61e1c427db3a3a5817e477106c5a56c
hargun.badhesha <hargunb2021@gmail.com>
2024-09-09 14:12:41 -0400

    Hi

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int number = 100;
+        int steps =0;
+        while(number>0){
+            if(number%2==0){
+                number = number/2;
+            }
+            else{
+                number--;
+            }
+            steps++;
+
+        }
+        System.out.println(steps);
+    }
+}
